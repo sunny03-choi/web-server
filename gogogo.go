41d324f2bcb580f2c627078849775dcc08686f78
sunny03-choi <sunny03.choi@gmail.com>
2020-04-08 15:32:50 +0900

    initialize

--- gogogo.go
+++ gogogo.go
@@ -7,5 +7,5 @@ import (
 func main() {
 	http.Handle("/", http.FileServer(http.Dir("wwwroot")))
 	//http.Handle("/static", http.FileServer(http.Dir("wwwroot")))
-	http.ListenAndServe(":6000", nil)
+	http.ListenAndServe(":2000", nil)
 }
