cfd15890d472f3cd72277abc646e633a6d5d3f25
sunny03-choi <sunny03.choi@gmail.com>
2020-04-08 14:52:10 +0900

    initialize

---
+++ ur.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"io/ioutil"
+	"net/http"
+	"path/filepath"
+)
+
+func main() {
+	http.Handle("/", new(staticHandler))
+	http.ListenAndServe("6336", nil)
+}
+
+type staticHandler struct {
+	http.Handler
+}
+
+func (h *staticHandler) ServeHTTP(w http.ResponseWriter, req *http.Request) {
+	localPath := "wwwroot" + req.URL.Path
+	content, err := ioutil.ReadFile(localPath)
+	if err != nil {
+		w.WriteHeader(404)
+		w.Write([]byte(http.StatusText(404)))
+	}
+	contentType := getContentType(localPath)
+	w.Header().Add("content-Type", contentType)
+	w.Write(content)
+}
+func getContentType(localPath string) string {
+	var contentType string
+	ext := filepath.Ext(localPath)
+	switch ext {
+	case ".html":
+		contentType = "text/html"
+	case ".css":
+		contentType = "text/css"
+	case ".js":
+		contentType = "application/javascript"
+	case "png":
+		contentType = "image/jpeg"
+	default:
+		contentType = "text/plain"
+	}
+	return contentType
+}
