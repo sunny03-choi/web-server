7449c9c5174a991715ca612341c4e2806cf8e094
sunny03-choi <sunny03.choi@gmail.com>
2020-04-10 14:59:44 +0900

    initialize

--- 21.go
+++ 21.go
@@ -1,1 +1,22 @@
 package main
+
+import (
+	"encoding/json"
+	"fmt"
+)
+
+type Member struct {
+	Name   string
+	Age    int
+	Active bool
+}
+
+func main() {
+	mem := Member{"Sunwoo", 18, true}
+	jsonBytes, err := json.Marshal(mem)
+	if err != nil {
+		panic(err)
+	}
+	jsonString := string(jsonBytes)
+	fmt.Println(jsonString)
+}
