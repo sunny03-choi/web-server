0a40337e446d0103f84cb6a8be8238c4b89dc8ac
sunny03-choi <sunny03.choi@gmail.com>
2020-04-11 17:44:13 +0900

    initialize

---
+++ google.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+)
+
+type Member struct {
+	Name   string
+	Age    int
+	Active bool
+}
+
+func main() {
+	jsonBytes, _ := json.Marshal(Member{"Tim", 1, true})
+	var mem Member
+	err := json.Unmarshal(jsonBytes, &mem)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println(mem.Name, mem.Age, mem.Active)
+}
