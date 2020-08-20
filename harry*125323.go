d2560f3d3eb495664948f0c52fbb8543757853d2
sunwoo <62876483+sunny03-choi@users.noreply.github.com>
2020-08-20 23:51:49 +0900

    Add files via upload

---
+++ harry*125323.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"database/sql"
+	"fmt"
+	_ "github.com/sunny03-choi/web-server"
+	"log"
+)
+
+func main()  {
+	db, err := sql.Open("web-server","root:pwd@tcp(127.0.0.1:3306)/testdb")
+	if err !=nil {
+		log.Fatal(err)
+	}
+	defer db.Close()
+	var name string
+	err = db.QueryRow("SELECT name FROM test1 WHERE id =1").Scan(&name)
+	if err !=nil {
+		log.Fatal(err)
+		}
+		fmt.Println(name)
+}
